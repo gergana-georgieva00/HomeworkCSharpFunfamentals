e0e1b69081a42a41130fe7b92efcbc8a5ef6f965
Gergana Georgieva <gerganagergana1@gmail.com>
2022-06-09 19:42:58 +0300

    More exercises

---
+++ PascalTriangle/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace PascalTriangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[][] jaggedArray = new int[n][];
+
+            for (int i = 0; i < n; i++)
+            {
+                jaggedArray[i] = new int[i + 1];
+            }
+
+            for (int row = 0; row < n; row++)
+            {
+                for (int col = 0; col <= row; col++)
+                {
+                    if (col == 0 || col == row)
+                    {
+                        jaggedArray[row][col] = 1;
+                    }
+                    else
+                    {
+                        jaggedArray[row][col] = jaggedArray[row - 1][col - 1] + jaggedArray[row - 1][col];
+                    }
+                }
+            }
+
+            for (int row = 0; row < n; row++)
+            {
+                for (int col = 0; col <= row; col++)
+                {
+                    Console.Write(jaggedArray[row][col] + " ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ RecursiveFibonacci/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace RecursiveFibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(GetFibonacci(number));
+        }
+
+        static int GetFibonacci(int n)
+        {
+            if (n <= 2)
+            {
+                return 1;
+            }
+            else
+            {
+                return GetFibonacci(n - 1) + GetFibonacci(n - 2);
+            }
+        }
+    }
+}
