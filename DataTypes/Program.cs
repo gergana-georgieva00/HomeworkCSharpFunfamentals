3b8c914a1c5699eb47e2d1910d774cb3653d1aca
Gergana Georgieva <gerganagergana1@gmail.com>
2022-06-16 15:23:44 +0300

    Methods - More Exercise

---
+++ DataTypes/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace DataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dataType = Console.ReadLine();
+            FindDataType(dataType);
+        }
+
+        static void FindDataType(string dataType)
+        {
+            int integer;
+            double realNum;
+            string someString;
+
+            string output = "";
+
+            switch (dataType)
+            {
+                case "int":
+                    integer = int.Parse(Console.ReadLine());
+                    integer *= 2;
+                    output = integer.ToString();
+                    break;
+                case "real":
+                    realNum = double.Parse(Console.ReadLine());
+                    realNum *= 1.5;
+                    output = string.Format($"{realNum:f2}");
+                    break;
+                default:
+                    someString = Console.ReadLine();
+                    output = string.Format($"${someString}$");
+                    break;
+            }
+
+            Console.WriteLine(output);
+        }
+    }
+}
