948cf327da9b68b342cbf7f66cfeb4804b56a3b9
Gergana Georgieva <gerganagergana1@gmail.com>
2022-06-17 14:53:14 +0300

    Homework - Lists

--- HouseParty/Program.cs
+++ HouseParty/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace HouseParty
 {
@@ -6,7 +7,46 @@ namespace HouseParty
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int num = int.Parse(Console.ReadLine());
+
+            List<string> guestList = new List<string>();
+            for (int i = 0; i < num; i++)
+            {
+                string input = Console.ReadLine();
+                string[] splitInput = input.Split(' ');
+
+                if (input.Contains("is not"))
+                {
+                    // if ont the list => delete
+                    if (guestList.Contains(splitInput[0]))
+                    {
+                        guestList.Remove(splitInput[0]);
+                    }
+                    // if not on the list => print a message
+                    else
+                    {
+                        Console.WriteLine($"{splitInput[0]} is not in the list!");
+                    }
+                }
+                else
+                {
+                    // if ont the list => print a message
+                    if (guestList.Contains(splitInput[0]))
+                    {
+                        Console.WriteLine($"{splitInput[0]} is already in the list!");
+                    }
+                    // if not on the list => add the name to the list
+                    else
+                    {
+                        guestList.Add(splitInput[0]);
+                    }
+                }
+            }
+
+            foreach (var guest in guestList)
+            {
+                Console.WriteLine(guest);
+            }
         }
     }
 }
---
+++ ListOperations/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ListOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> integers = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+
+            string command;
+            while ((command = Console.ReadLine()) != "End")
+            {
+                string[] splitCommand = command.Split(' ');
+                // add the given number to the end of the list
+                if (command.Contains("Add"))
+                {
+                    integers.Add(int.Parse(splitCommand[1]));
+                }
+                // insert the number at the given index
+                else if (command.Contains("Insert"))
+                {
+                    if ((int.Parse(splitCommand[2]) > integers.Count - 1) || int.Parse(splitCommand[2]) < 0)
+                    {
+                        Console.WriteLine("Invalid index");
+                    }
+                    else
+                    {
+                        integers.Insert(int.Parse(splitCommand[2]), int.Parse(splitCommand[1]));
+                    }
+                }
+                // remove the number at the given index
+                else if (command.Contains("Remove"))
+                {
+                    if ((int.Parse(splitCommand[1]) > integers.Count - 1) || int.Parse(splitCommand[1]) < 0)
+                    {
+                        Console.WriteLine("Invalid index");
+                    }
+                    else
+                    {
+                        integers.RemoveAt(int.Parse(splitCommand[1]));
+                    }
+                   
+                }
+                // first number becomes last. This has to be repeated the specified number of times
+                else if (command.Contains("left"))
+                {
+                    for (int i = 0; i < int.Parse(splitCommand[2]); i++)
+                    {
+                        int movingInt = integers[0];
+                        integers.RemoveAt(0);
+                        integers.Add(movingInt);
+                    }
+                }
+                // last number becomes first. To be repeated the specified number of times
+                else if (command.Contains("right"))
+                {
+                    for (int i = 0; i < int.Parse(splitCommand[2]); i++)
+                    {
+                        int lastIndex = integers.Count - 1;
+                        int movingInt = integers[lastIndex];
+                        integers.RemoveAt(lastIndex);
+                        integers.Insert(0, movingInt);
+                    }
+                }
+            }
+
+            Console.WriteLine(string.Join(' ', integers));
+        }
+    }
+}
