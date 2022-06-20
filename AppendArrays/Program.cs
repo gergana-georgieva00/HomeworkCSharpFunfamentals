51185b580e6ceba03c93eb9f66eb2ce980f9caa9
Gergana Georgieva <gerganagergana1@gmail.com>
2022-06-20 16:11:46 +0300

    Homework Lists

---
+++ AppendArrays/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace AppendArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> input = Console.ReadLine().Split('|').Reverse().ToList();
+
+            List<int> numbers = new List<int>();
+            foreach (var item in input)
+            {
+                numbers.AddRange(item.Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList());
+            }
+
+            Console.WriteLine(string.Join(' ', numbers));
+        }
+    }
+}
