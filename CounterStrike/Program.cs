242613dfef1a15c885548608232acf9f32df0cc2
Gergana Georgieva <gerganagergana1@gmail.com>
2022-06-23 15:16:52 +0300

    Mid Exam Preparation

---
+++ CounterStrike/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace CounterStrike
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long initialEnergy = long.Parse(Console.ReadLine());
+
+            bool notEhoughEnergy = false;
+            long wonBattles = 0;
+            string command;
+
+            while ((command = Console.ReadLine()) != "End of battle")
+            {
+                long distance = long.Parse(command);
+
+                if (initialEnergy - distance < 0)
+                {
+                    notEhoughEnergy = true;
+                    break;
+                }
+
+                initialEnergy -= distance;
+
+                wonBattles++;
+
+                if (wonBattles % 3 == 0)
+                {
+                    initialEnergy += wonBattles;
+                }
+            }
+
+            if (notEhoughEnergy)
+            {
+                Console.WriteLine($"Not enough energy! Game ends with {wonBattles} won battles and {initialEnergy} energy");
+            }
+            else
+            {
+                Console.WriteLine($"Won battles: {wonBattles}. Energy left: {initialEnergy}");
+            }
+        }
+    }
+}
