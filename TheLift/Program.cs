11b16a1bf088a9839ce7aff9831c9c12b18cf0ee
Gergana Georgieva <gerganagergana1@gmail.com>
2022-06-23 12:35:22 +0300

    Mid Exam Preparation

---
+++ TheLift/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TheLift
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int peopleWaiting = int.Parse(Console.ReadLine());
+            List<int> currentState = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+
+            int index = 0;
+            
+            while (peopleWaiting > 0)
+            {
+                if (index >= currentState.Count)
+                {
+                    break;
+                }
+
+                if (currentState[index] < 4)
+                {
+                    currentState[index]++;
+                    peopleWaiting--;
+                }
+                else
+                {
+                    index++;
+                }
+                
+            }
+
+            if (peopleWaiting <= 0)
+            {
+                if (currentState[currentState.Count - 1] < 4)
+                {
+                    Console.WriteLine("The lift has empty spots!");
+                }
+            }
+            else
+            {
+                Console.WriteLine($"There isn't enough space! {peopleWaiting} people in a queue!");
+            }
+
+            Console.WriteLine(string.Join(' ', currentState));
+        }
+    }
+}
