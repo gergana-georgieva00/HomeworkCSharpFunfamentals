7ca3f0bfac4fde795e16c535b68c973cbf3cb74f
Gergana Georgieva <gerganagergana1@gmail.com>
2022-07-01 15:15:30 +0300

    Objects and classes - lab

---
+++ Songs/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Songs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            List<Song> songs = new List<Song>();
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split('_');
+
+                Song song = new Song(input[0], input[1], input[2]);
+                songs.Add(song);
+            }
+
+            string command = Console.ReadLine();
+            if (command == "all")
+            {
+                foreach (Song song in songs)
+                {
+                    Console.WriteLine(song.Name);
+                }
+            }
+            else
+            {
+                foreach (Song song in songs)
+                {
+                    if (song.TypeList == command)
+                    {
+                        Console.WriteLine(song.Name);
+                    }
+                }
+            }
+        }
+    }
+
+    class Song
+    {
+        public Song(string typeList, string name, string time)
+        {
+            this.TypeList = typeList;
+            this.Name = name;
+            this.Time = time;
+        }
+
+        public string TypeList { get; set; }
+        public string Name { get; set; }
+        public string Time { get; set; }
+
+
+    }
+}
