bd86bc182c9aeb0b1185002fb77feea52f58acb6
Gergana Georgieva <gerganagergana1@gmail.com>
2022-07-04 15:08:15 +0300

    Homework - objects and classes

---
+++ AdvertisementMessage/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AdvertisementMessage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> phrases = new List<string>
+            {
+                "Excellent product.", 
+                "Such a great product.", 
+                "I always use that product.", 
+                "Best product of its category.", 
+                "Exceptional product.", 
+                "I can’t live without this product."
+            };
+
+            List<String> events = new List<string>
+            {   "Now I feel good.", 
+                "I have succeeded with this product.", 
+                "Makes miracles. I am happy of the results!", 
+                "I cannot believe but now I feel awesome.", 
+                "Try it yourself, I am very satisfied.", 
+                "I feel great!"
+            };
+
+            List<string> authors = new List<string>
+            {"Diana", "Petya", "Stella", "Elena", "Katya", "Iva", "Annie", "Eva"};
+
+            List<string> cities = new List<string>
+            {"Burgas", "Sofia", "Plovdiv", "Varna", "Ruse"};
+
+            int n = int.Parse(Console.ReadLine());
+
+            Random random = new Random();
+            for (int i = 0; i < n; i++)
+            {
+                int randomPhrase = random.Next(phrases.Count);
+                int randomEvent = random.Next(events.Count);
+                int randomAuthor = random.Next(authors.Count);
+                int randomCity = random.Next(cities.Count);
+
+                Console.WriteLine($"{phrases[randomPhrase]} {events[randomEvent]} {authors[randomAuthor]} – {cities[randomCity]}.");
+            }
+        }
+    }
+}
