8333b8a4bd8982d8c4fc059bff9aa98046349564
Gergana Georgieva <gerganagergana1@gmail.com>
2022-07-04 14:16:33 +0300

    Objects and classes - lab

---
+++ StoreBoxes/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StoreBoxes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Box> boxes = new List<Box>();
+
+            string command;
+            while ((command = Console.ReadLine()) != "end")
+            {
+                string[] input = Console.ReadLine().Split(' ');
+
+                Box box = new Box
+                {
+                    SerialNumber = input[0],
+                    Item = new Item
+                    {
+                        Name = input[1],
+                        Price = decimal.Parse(input[3])
+                    },
+                    ItemQuantity = int.Parse(input[2])
+                };
+
+                boxes.Add(box);
+            }
+
+            foreach (Box box in boxes.OrderByDescending(box => box.PriceBox))
+            {
+                Console.WriteLine(box.SerialNumber);
+                Console.WriteLine($"-- {box.Item.Name} – ${box.Item.Price:f2}: {box.ItemQuantity}");
+                Console.WriteLine($"-- ${box.PriceBox:f2}");
+            }
+        }
+    }
+
+    class Item
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+
+    class Box
+    {
+        public string SerialNumber { get; set; }
+        public Item Item { get; set; }
+        public int ItemQuantity { get; set; }
+        public decimal PriceBox 
+        {
+            get 
+            {
+                return this.ItemQuantity * this.Item.Price;
+            } 
+        }
+    }
+}
---
+++ VehicleCatalogue/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace VehicleCatalogue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Catalog catalog = new Catalog();
+
+            string command;
+            while ((command = Console.ReadLine()) != "end")
+            {
+                string[] splitCom = command.Split('/');
+
+                if (splitCom[0] == "Car")
+                {
+                    Car car = new Car
+                    {
+                        Brand = splitCom[1],
+                        Model = splitCom[2],
+                        HorsePower = double.Parse(splitCom[3])
+                    };
+
+                    catalog.Cars.Add(car);
+                }
+                else
+                {
+                    Truck truck = new Truck
+                    {
+                        Brand = splitCom[1],
+                        Model = splitCom[2],
+                        Weight = double.Parse(splitCom[3])
+                    };
+
+                    catalog.Trucks.Add(truck);
+                }
+            }
+
+            Console.WriteLine("Cars:");
+            foreach (var car in catalog.Cars.OrderBy(car => car.Brand))
+            {
+                Console.WriteLine($"{car.Brand}: {car.Model} - {car.HorsePower}hp");
+            }
+
+            Console.WriteLine("Trucks:");
+            foreach (var truck in catalog.Trucks.OrderBy(truck => truck.Brand))
+            {
+                Console.WriteLine($"{truck.Brand}: {truck.Model} - {truck.Weight}kg");
+            }
+        }
+    }
+
+    class Truck
+    {
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public double Weight { get; set; }
+    }
+
+    class Car
+    {
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public double HorsePower { get; set; }
+    }
+
+    class Catalog
+    {
+        public Catalog()
+        {
+            this.Cars = new List<Car>();
+            this.Trucks = new List<Truck>();
+        }
+        public List<Truck> Trucks { get; set; }
+        public List<Car> Cars { get; set; }
+    }
+}
