b475d4da188a8a02939bb96d8d6ee6bc755b9990
Gergana Georgieva <gerganagergana1@gmail.com>
2022-07-05 21:10:02 +0300

    Objects and classes - more exercise

---
+++ OldestFamilyMember/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OldestFamilyMember
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Family family = new Family() 
+            {
+                People = new List<Person>()
+            };
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split(' ');
+                string name = input[0];
+                int age = int.Parse(input[1]);
+
+                Person person = new Person(name, age);
+
+                family.AddMember(person);
+            }
+
+            Person oldestMember = family.GetOldestMember(family.People);
+
+            Console.WriteLine($"{oldestMember.Name} {oldestMember.Age}");
+        }
+    }
+
+    class Person
+    {
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+
+    class Family
+    {
+        public Family()
+        {
+            List<Person> People = new List<Person>();
+        }
+
+        public List<Person> People { get; set; }
+
+        public void AddMember(Person member)
+        {
+            People.Add(member);
+        }
+
+        public Person GetOldestMember(List<Person> People)
+        {
+            Person oldestPerson = People.OrderByDescending(x => x.Age).FirstOrDefault();
+
+            return oldestPerson;
+        }
+    }
+}
