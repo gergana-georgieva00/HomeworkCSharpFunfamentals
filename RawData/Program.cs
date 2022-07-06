de023d47167d4db0cbb376ae469689a68d8f55c8
Gergana Georgieva <gerganagergana1@gmail.com>
2022-07-06 13:40:21 +0300

    Objects and classes - more exercise

---
+++ RawData/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RawData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            List<Car> cars = new List<Car>();
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split(' ');
+                string model = input[0];
+                int speed = int.Parse(input[1]);
+                int power = int.Parse(input[2]);
+                int weight = int.Parse(input[3]);
+                string type = input[4];
+
+                Car car = new Car
+                    (
+                        model,
+                        new Engine(speed, power),
+                        new Cargo(weight, type)
+                    );
+                cars.Add(car);
+            }
+
+            string command = Console.ReadLine();
+
+            if (command == "fragile")
+            {
+                foreach (var car in cars)
+                {
+                    if (car.Cargo.Type == "fragile" && car.Cargo.Weight < 1000)
+                    {
+                        Console.WriteLine(car.Model);
+                    }
+                }
+            }
+            else
+            {
+                foreach (var car in cars)
+                {
+                    if (car.Cargo.Type == "flamable" && car.Engine.Power > 250)
+                    {
+                        Console.WriteLine(car.Model);
+                    }
+                }
+            }
+        }
+    }
+
+    class Car
+    {
+        public Car(string model, Engine engine, Cargo cargo)
+        {
+            this.Model = model;
+            this.Engine = engine;
+            this.Cargo = cargo;
+        }
+
+        public string Model { get; set; }
+        public Engine Engine { get; set; }
+        public Cargo Cargo { get; set; }
+    }
+
+    class Engine
+    {
+        public Engine(int speed, int power)
+        {
+            this.Speed = speed;
+            this.Power = power;
+        }
+
+        public int Speed { get; set; }
+        public int Power { get; set; }
+    }
+
+    class Cargo
+    {
+        public Cargo(int weight, string type)
+        {
+            this.Weight = weight;
+            this.Type = type;
+        }
+
+        public int Weight { get; set; }
+        public string Type { get; set; }
+    }
+}
