bc1144da52c52eb05cd91b3a664bbaadede0dc55
Gergana Georgieva <gerganagergana1@gmail.com>
2022-07-10 14:58:33 +0300

    Associative arrays - lab

---
+++ CountRealNumbers/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CountRealNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<double> numbers = Console.ReadLine().Split(' ').Select(double.Parse).ToList();
+            numbers.Sort();
+
+            Dictionary<double, int> numsWithOccurrences = new Dictionary<double, int>();
+
+            foreach (var number in numbers)
+            {
+                if (numsWithOccurrences.ContainsKey(number))
+                {
+                    numsWithOccurrences[number] += 1;
+                }
+                else
+                {
+                    numsWithOccurrences.Add(number, 1);
+                }
+            }
+
+            foreach (var currPair in numsWithOccurrences)
+            {
+                Console.WriteLine($"{currPair.Key} -> {currPair.Value}");
+            }
+        }
+    }
+}
