325bea585d2aed86864cb3bc544dabdc6ed73cff
Gergana Georgieva <gerganagergana1@gmail.com>
2022-07-10 16:46:18 +0300

    Linq exercise

---
+++ WordFilter/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace WordFilter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arr = Console.ReadLine()
+                            .Split()
+                            .Where(w => w.Length % 2 == 0)
+                            .ToArray();
+
+            foreach (var word in arr)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
