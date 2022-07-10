0174c5b4a55e8d54d2501c40930ed55a23c8f24a
Gergana Georgieva <gerganagergana1@gmail.com>
2022-07-10 16:26:09 +0300

    Associative arrays - lab

---
+++ OddOccurrences/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OddOccurrences
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> words = Console.ReadLine().Split().ToList();
+
+            Dictionary<string, int> wordsWithOccurrences = new Dictionary<string, int>();
+
+            foreach (var word in words)
+            {
+                if (wordsWithOccurrences.ContainsKey(word.ToLower()))
+                {
+                    wordsWithOccurrences[word.ToLower()]++;
+                }
+                else
+                {
+                    wordsWithOccurrences.Add(word.ToLower(), 1);
+                }
+            }
+
+            foreach (var wordOccPair in wordsWithOccurrences)
+            {
+                if (!(wordOccPair.Value % 2 == 0))
+                {
+                    Console.Write(wordOccPair.Key + " ");
+                }
+            }
+        }
+    }
+}
