fa8aeea3047c2f4b5f5a90e29ef5146db24f9bcf
Gergana Georgieva <gerganagergana1@gmail.com>
2022-07-10 16:41:11 +0300

    Associative arrays - lab

---
+++ WordSynomyms/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WordSynomyms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Dictionary<string, List<string>> wordSynonyms = new Dictionary<string, List<string>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string word = Console.ReadLine();
+                string synonym = Console.ReadLine();
+
+                if (wordSynonyms.ContainsKey(word))
+                {
+                    wordSynonyms[word].Add(synonym);
+                }
+                else
+                {
+                    wordSynonyms[word] = new List<string>();
+                    wordSynonyms[word].Add(synonym);
+                }
+            }
+
+            foreach (var wordSynonymPair in wordSynonyms)
+            {
+                Console.Write(wordSynonymPair.Key + " - ");
+                Console.WriteLine(string.Join(", ", wordSynonymPair.Value));
+            }
+        }
+    }
+}
