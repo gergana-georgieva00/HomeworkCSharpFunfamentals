fffed3362b410021baedf266732e1a43fe94c01a
Gergana Georgieva <gerganagergana1@gmail.com>
2022-07-11 19:29:54 +0300

    Dictionaries - homework

---
+++ StudentAcademy/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StudentAcademy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Dictionary<string, List<double>> studentsAndGrades = new Dictionary<string, List<double>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string name = Console.ReadLine();
+                double grade = double.Parse(Console.ReadLine());
+
+                if (!studentsAndGrades.ContainsKey(name))
+                {
+                    studentsAndGrades.Add(name, new List<double>());
+                }
+                
+                studentsAndGrades[name].Add(grade);
+            }
+
+            foreach (var pair in studentsAndGrades)
+            {
+                if (pair.Value.AsQueryable().Sum() / pair.Value.Count < 4.5)
+                {
+                    studentsAndGrades.Remove(pair.Key);
+                }
+            }
+
+            foreach (var pair in studentsAndGrades)
+            {
+                Console.WriteLine($"{pair.Key} -> {(pair.Value.AsQueryable().Sum() / pair.Value.Count):f2}");
+            }
+        }
+    }
+}
