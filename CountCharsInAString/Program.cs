ec7c25de4d6e259feb83390d7986ff4de7c9e63f
Gergana Georgieva <gerganagergana1@gmail.com>
2022-07-11 15:29:23 +0300

    Associative arrays - homework

---
+++ CountCharsInAString/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CountCharsInAString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] input = Console.ReadLine().ToCharArray();
+
+            Dictionary<char, int> occurrencesOfChars = new Dictionary<char, int>();
+
+            foreach (var currChar in input)
+            {
+                if (currChar == ' ')
+                {
+                    continue;
+                }
+
+                if (occurrencesOfChars.ContainsKey(currChar))
+                {
+                    occurrencesOfChars[currChar]++;
+                }
+                else
+                {
+                    occurrencesOfChars.Add(currChar, 1);
+                }
+            }
+
+            foreach (var charOccPair in occurrencesOfChars)
+            {
+                Console.WriteLine($"{charOccPair.Key} -> {charOccPair.Value}");
+            }
+        }
+    }
+}
