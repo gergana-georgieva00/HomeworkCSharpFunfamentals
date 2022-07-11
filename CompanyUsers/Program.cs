3c121344293da51412b054902bedc8d0ff9c1147
Gergana Georgieva <gerganagergana1@gmail.com>
2022-07-11 19:38:35 +0300

    Associative arrays - homework

---
+++ CompanyUsers/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CompanyUsers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<string>> companyEmployee = new Dictionary<string, List<string>>();
+
+            string input;
+            while ((input = Console.ReadLine()) != "End")
+            {
+                string company = input.Split(" -> ")[0];
+                string id = input.Split(" -> ")[1];
+
+                if (!companyEmployee.ContainsKey(company))
+                {
+                    companyEmployee.Add(company, new List<string>());
+                    companyEmployee[company].Add(id);
+                }
+                else
+                {
+                    if (!companyEmployee[company].Contains(id))
+                    {
+                        companyEmployee[company].Add(id);
+                    }
+                }
+            }
+
+            foreach (var pair in companyEmployee)
+            {
+                Console.WriteLine($"{pair.Key}");
+
+                foreach (var id in pair.Value)
+                {
+                    Console.WriteLine($"-- {id}");
+                }
+            }
+        }
+    }
+}
