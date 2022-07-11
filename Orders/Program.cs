f56b95869bb7b3a1208d9e8e24677a91e9cd9d70
Gergana Georgieva <gerganagergana1@gmail.com>
2022-07-11 18:44:17 +0300

    Assiciative arrays - homework

--- Orders/Program.cs
+++ Orders/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Orders
 {
@@ -6,21 +8,54 @@ namespace Orders
     {
         static void Main(string[] args)
         {
-            int n = int.Parse(Console.ReadLine());
+            List<Product> products = new List<Product>();
 
-            double total = 0.0;
-            for (int i = 0; i < n; i++)
+            string command;
+            while ((command = Console.ReadLine()) != "buy")
             {
-                double pricePerCapsule = double.Parse(Console.ReadLine());
-                int days = int.Parse(Console.ReadLine());
-                double capsuleCount = double.Parse(Console.ReadLine());
+                string name = command.Split()[0];
+                double price = double.Parse(command.Split()[1]);
+                double quantity = double.Parse(command.Split()[2]);
 
-                double currPriceCoffee = pricePerCapsule * (double)days * capsuleCount;
-                Console.WriteLine($"The price for the coffee is: ${currPriceCoffee:f2}");
-                total += currPriceCoffee;
+                Product product = new Product(name, price, quantity);
+
+                if (products.Any(p => p.Name == name))
+                {
+                    Product currProduct = products.Find(p => p.Name == name);
+
+                    foreach (var item in products)
+                    {
+                        if (item.Name == name)
+                        {
+                            item.Price = price;
+                            item.Quantity += quantity;
+                        }
+                    }
+                }
+                else
+                {
+                    products.Add(product);
+                }
             }
 
-            Console.WriteLine($"Total: ${total:f2}");
+            foreach (Product product in products)
+            {
+                Console.WriteLine($"{product.Name} -> {(product.Quantity * product.Price):f2}");
+            }
         }
+    }
+
+    class Product
+    {
+        public Product(string name, double price, double quantity)
+        {
+            this.Name = name;
+            this.Price = price;
+            this.Quantity = quantity;
+        }
+
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public double Quantity { get; set; }
     }
 }
