ec5d0f2425f6a784c551ac1984fb66994fc31cbf
Gergana Georgieva <gerganagergana1@gmail.com>
2022-07-11 19:19:39 +0300

    Associative arrays - homework

---
+++ Courses/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Courses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<string>> courses = new Dictionary<string, List<string>>();
+
+            string command;
+            while ((command = Console.ReadLine()) != "end")
+            {
+                string course = command.Split(" : ")[0];
+                string studentName = command.Split(" : ")[1];
+
+                if (!courses.ContainsKey(course))
+                {
+                    courses.Add(course, new List<string>());
+                }
+                
+                courses[course].Add(studentName);
+            }
+
+            foreach (var pair in courses)
+            {
+                Console.WriteLine($"{pair.Key}: {pair.Value.Count}");
+
+                foreach (var student in pair.Value)
+                {
+                    Console.WriteLine($"-- {student}");
+                }
+            }
+        }
+    }
+}
