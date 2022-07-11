16984422155a5a0579c7840d1ffa6e5794ad12c1
Gergana Georgieva <gerganagergana1@gmail.com>
2022-07-11 15:41:07 +0300

    Assotiative arrays - homework

---
+++ AMinerTask/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AMinerTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            Dictionary<string, long> resourceQuantity = new Dictionary<string, long>();
+
+            while ((input = Console.ReadLine()) != "stop")
+            {
+                string resource = input;
+                long quantity = long.Parse(Console.ReadLine());
+
+                if (resourceQuantity.ContainsKey(resource))
+                {
+                    resourceQuantity[resource] += quantity;
+                }
+                else
+                {
+                    resourceQuantity.Add(resource, quantity);
+                }
+            }
+
+            foreach (var pair in resourceQuantity)
+            {
+                Console.WriteLine($"{pair.Key} -> {pair.Value}");
+            }
+        }
+    }
+}
