3e5753b0c70d779f171e08e8f34e0737693a198e
Gergana Georgieva <gerganagergana1@gmail.com>
2022-07-25 01:25:26 +0300

    Text processing - lab

---
+++ TextFilter/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace TextFilter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] bannedWords = Console.ReadLine().Split(", ");
+            string input = Console.ReadLine();
+
+            foreach (var word in bannedWords)
+            {
+                while (input.Contains(word))
+                {
+                    input = input.Replace(word, new string('*', word.Length));
+                }
+            }
+
+            Console.WriteLine(input);
+        }
+    }
+}
