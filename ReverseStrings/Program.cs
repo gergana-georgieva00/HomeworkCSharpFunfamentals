b71cfd5a842853c8003b4a4b3df379fa6b092756
Gergana Georgieva <gerganagergana1@gmail.com>
2022-07-25 00:14:22 +0300

    Text processing - lab

---
+++ ReverseStrings/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ReverseStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string command;
+            while ((command = Console.ReadLine()) != "end")
+            {
+                Console.Write($"{command} = ");
+                char[] arr = command.ToCharArray();
+                Array.Reverse(arr);
+                Console.WriteLine($"{new string(arr)}");
+            }
+        }
+    }
+}
