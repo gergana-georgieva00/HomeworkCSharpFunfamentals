9933993f7c19b75d8eea58e63b13a7031f2865de
Gergana Georgieva <gerganagergana1@gmail.com>
2022-07-25 00:26:39 +0300

    Text processing - lab

---
+++ RepeatStrings/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace RepeatStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arr = Console.ReadLine().Split();
+
+            string result = "";
+
+            foreach (var word in arr)
+            {
+                int length = word.Length;
+                result += string.Concat((Enumerable.Repeat(word, length)));
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
