5dd24bc5521e47e432ea7423bee0e51788e24bb3
Gergana Georgieva <gerganagergana1@gmail.com>
2022-07-25 00:44:39 +0300

    Text processing - lab

---
+++ Substring/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Substring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string match = Console.ReadLine();
+            string input = Console.ReadLine();
+
+            while (input.Contains(match))
+            {
+                input = input.Remove(input.IndexOf(match), match.Length);
+            }
+
+            Console.WriteLine(input);
+        }
+    }
+}
