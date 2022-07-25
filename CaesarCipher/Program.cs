cd0395aebb2a7abb05471d8acd6e08896377122e
Gergana Georgieva <gerganagergana1@gmail.com>
2022-07-25 15:57:08 +0300

    Text processing - homework

---
+++ CaesarCipher/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CaesarCipher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] input = Console.ReadLine().ToCharArray();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                input[i] = Convert.ToChar(input[i] + 3);
+            }
+
+            foreach (var character in input)
+            {
+                Console.Write(character);
+            }
+        }
+    }
+}
