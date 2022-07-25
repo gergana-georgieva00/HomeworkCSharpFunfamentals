c647c19f5f0a2f6520a09c8dee67cf3b412f2a72
Gergana Georgieva <gerganagergana1@gmail.com>
2022-07-25 13:47:53 +0300

    Text processing - homework

---
+++ ExtractFile/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ExtractFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            int startIndex = input.LastIndexOf('\\') + 1;
+            string cut = input.Substring(startIndex);
+
+            string fileName = cut.Split('.')[0];
+            string extension = cut.Split('.')[1];
+
+            Console.WriteLine($"File name: {fileName}");
+            Console.WriteLine($"File extension: {extension}");
+        }
+    }
+}
