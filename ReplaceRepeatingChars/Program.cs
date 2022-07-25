aed8265c5bf9e9ad1ddeb9d7d5c350c6b7adacd4
Gergana Georgieva <gerganagergana1@gmail.com>
2022-07-25 16:49:00 +0300

    Text processing - homework

---
+++ ReplaceRepeatingChars/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ReplaceRepeatingChars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            List<char> output = new List<char>();
+
+            foreach (var element in input.ToCharArray())
+            {
+                if (output.Count == 0 || output.Last() != element)
+                    output.Add(element);
+            }
+
+            var result = new string(output.ToArray());
+
+            Console.WriteLine(string.Join("", output));
+        }
+    }
+}
