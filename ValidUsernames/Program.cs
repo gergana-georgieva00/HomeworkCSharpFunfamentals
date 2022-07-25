ebe3ecd50c97d89876749e1b458fe44e521ec495
Gergana Georgieva <gerganagergana1@gmail.com>
2022-07-25 12:23:27 +0300

    Text processing - homework

---
+++ ValidUsernames/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ValidUsernames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] usernames = Console.ReadLine().Split(", ");
+
+            foreach (var username in usernames)
+            {
+                bool validLength = false;
+                bool validChars = true;
+
+                if (username.Length >= 3 && username.Length <= 16)
+                {
+                    validLength = true;
+                }
+                else
+                {
+                    continue;
+                }
+
+                foreach (var currChar in username)
+                {
+                    if (!Char.IsLetterOrDigit(currChar) && currChar != '-' && currChar != '_')
+                    {
+                        validChars = false;
+                        break;
+                    }
+                }
+                
+
+                if (validLength && validChars)
+                {
+                    Console.WriteLine(username);
+                }
+            }
+        }
+    }
+}
