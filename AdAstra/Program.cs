5d7a04f9251add6195c7bfe561b10a89831f4923
Gergana Georgieva <gerganagergana1@gmail.com>
2022-07-27 14:50:27 +0300

    Regex - lab

--- AdAstra/Program.cs
+++ AdAstra/Program.cs
@@ -7,32 +7,7 @@ namespace AdAstra
     {
         static void Main(string[] args)
         {
-            PrintInParallel();  
+
         } 
-
-        private static void PrintInParallel()
-        {
-            var newThread = new Thread(() => 
-            {
-                for (int i = 0; i < 1000; i++)
-                {
-                    Console.Write("x");
-                }
-            });
-
-            newThread.Start();
-
-            for (int i = 0; i < 1000; i++)
-            {
-                Console.Write("y");
-            }
-
-            // When doing work on two threads the reasult is undeterministic
-            // we can't know which task is going to be done first
-            // in the example we can see in the console that both are printing in the same time
-            // the cpu is giving a little time to each thread and thats why we see chunks of x and chunks of y iterating one after
-            // another, but that is not in our controll, we can not say for cirtain which one will start printing first, or if one isn't going 
-            // to finish before the other. 
-        }
     }
 }
---
+++ Regex - lab/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Regex___lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "_ (Underscores) are also word characters!";
+
+            Regex regex = new Regex(@"[a-zA-Z]");
+            MatchCollection matches = regex.Matches(text);
+
+            foreach (var match in matches)
+            {
+                Console.WriteLine(match);
+            }
+        }
+    }
+}
