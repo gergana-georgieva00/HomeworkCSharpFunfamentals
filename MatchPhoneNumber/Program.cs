2502e085249d4d2e2823a8091cdf5ad01d936eac
Gergana Georgieva <gerganagergana1@gmail.com>
2022-07-27 17:19:20 +0300

    Regex - lab

---
+++ MatchPhoneNumber/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace MatchPhoneNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string phones = Console.ReadLine();
+
+            Regex regex = new Regex(@"\+359( |-)2\1(\d){3}\1(\d){4}\b");
+
+            MatchCollection matches = regex.Matches(phones);
+
+            Console.WriteLine(string.Join(", ", matches));
+        }
+    }
+}
