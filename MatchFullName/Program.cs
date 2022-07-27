e459b767fa3d9eca664843fb8cdf4e22a98aef0c
Gergana Georgieva <gerganagergana1@gmail.com>
2022-07-27 17:12:16 +0300

    Regex - lab

---
+++ MatchFullName/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace MatchFullName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string names = Console.ReadLine();
+
+            Regex regex = new Regex(@"\b([A-Z][a-z]+) ([A-Z][a-z]+)\b");
+            MatchCollection matches = regex.Matches(names);
+
+            foreach (var match in matches)
+            {
+                Console.Write(match + " ");
+            }
+        }
+    }
+}
