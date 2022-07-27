09341db57bf6eba1fb0e1602acb2a6b98809bc94
Gergana Georgieva <gerganagergana1@gmail.com>
2022-07-27 15:57:47 +0300

    Regex - lab

---
+++ CaptureTheValueOfNumberedGroup/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace CaptureTheValueOfNumberedGroup
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "<b>Regular Expressions</b> <p>I am a paragraph</p> ... some text after <span>Hello, I am Span</span> <a href=\"https://softuni.bg/\">SoftUni>/a>";
+
+            Regex regex = new Regex(@"<(.+)[^>]*>(.*?)<\/\1>");
+            MatchCollection matches = regex.Matches(text);
+
+            foreach (var match in matches)
+            {
+                Console.WriteLine(match);
+            }
+        }
+    }
+}
