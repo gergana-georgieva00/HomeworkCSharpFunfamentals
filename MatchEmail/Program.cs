5e3bfb1b055cb42c6d288d476304ce3504588e11
Gergana Georgieva <gerganagergana1@gmail.com>
2022-07-27 15:11:16 +0300

    Regex - lab

---
+++ MatchEmail/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace MatchEmail
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "valid123@email.bg invalid*name@emai1.bg";
+
+            Regex regex = new Regex(@"[a-zA-Z0-9_]+@[a-zA-Z]+\.[a-zA-Z]+");
+            MatchCollection matches = regex.Matches(text);
+
+            foreach (var match in matches)
+            {
+                Console.WriteLine(match);
+            }
+        }
+    }
+}
