05fc120d149ddf1642933c5cc64d1e5dc2ee8234
Gergana Georgieva <gerganagergana1@gmail.com>
2022-07-31 20:21:20 +0300

    Regex - homework

---
+++ ExtractEmails/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace ExtractEmails
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(new char[] { ' ', ','});
+
+            List<string> emails = new List<string>();
+
+            string regex = @"(?<user>^[a-zA-Z0-9]+([\.\-_])?[a-zA-Z0-9]*)@(?<host>[a-zA-Z]+(-)?[a-zA-Z]*(\.[a-zA-Z]+(-)?[a-zA-Z]+)+)";
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                MatchCollection matches = Regex.Matches(input[i], regex);
+
+                if (matches.Count > 0)
+                {
+                    foreach (Match match in matches)
+                    {
+                        emails.Add(match.Value);
+                    }
+                }
+            }
+
+            Console.WriteLine(string.Join(Environment.NewLine, emails));
+        }
+    }
+}
