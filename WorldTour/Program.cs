7b89a0df6632c214f570d7dfa3ab79535555c651
Gergana Georgieva <gerganagergana1@gmail.com>
2022-08-02 14:46:55 +0300

    Exam Preparation

---
+++ WorldTour/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WorldTour
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
