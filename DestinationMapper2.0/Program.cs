5d9e1a5aaa0bb8256b644007521ae10e405e2655
Gergana Georgieva <gerganagergana1@gmail.com>
2022-08-06 21:43:07 +0300

    Exam preparation

---
+++ DestinationMapper2.0/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace DestinationMapper2._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string places = Console.ReadLine();
+            string regex = @"(=|\/)(?<destination>[A-Z][a-zA-Z]{2,})\1";
+
+            List<string> destinations = new List<string>();
+            int points = 0;
+
+            MatchCollection matches = Regex.Matches(places, regex);
+            foreach (Match match in matches)
+            {
+                destinations.Add(match.Groups["destination"].Value);
+                points += match.Groups["destination"].Value.Length;
+            }
+
+            Console.WriteLine($"Destinations: {string.Join(", ", destinations)}");
+            Console.WriteLine($"Travel Points: {points}");
+        }
+    }
+}
