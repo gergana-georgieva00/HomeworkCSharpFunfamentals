546248a7ed1bcc15bec85fd9345005b8b780c9c7
Gergana Georgieva <gerganagergana1@gmail.com>
2022-08-06 21:20:28 +0300

    Exam Preparation

---
+++ WorldTour2.0/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace WorldTour2._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string stops = Console.ReadLine();
+
+            string command;
+            while ((command = Console.ReadLine()) != "Travel")
+            {
+                string[] splCommand = command.Split(':');
+
+                if (command.Contains("Add"))
+                {
+                    int index = int.Parse(splCommand[1]);
+                    string str = splCommand[2];
+
+                    if (index >= 0 && index < stops.Length)
+                    {
+                        stops = stops.Insert(index, str);
+                    }
+
+                    Console.WriteLine(stops);
+                }
+                else if (command.Contains("Remove"))
+                {
+                    int startIndex = int.Parse(splCommand[1]);
+                    int endIndex = int.Parse(splCommand[2]);
+
+                    if (startIndex >= 0 && endIndex < stops.Length)
+                    {
+                        stops = stops.Remove(startIndex, endIndex - startIndex + 1);
+                    }
+
+                    Console.WriteLine(stops);
+                }
+                else
+                {
+                    string oldStr = splCommand[1];
+                    string newStr = splCommand[2];
+
+                    if (stops.Contains(oldStr))
+                    {
+                        stops = stops.Replace(oldStr, newStr);
+                    }
+
+                    Console.WriteLine(stops);
+                }
+            }
+
+            Console.WriteLine($"Ready for world tour! Planned stops: {stops}");
+        }
+    }
+}
